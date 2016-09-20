29d3e99dad5b31aacda0757a007f211071f66840
Martin Keegan <martin@no.ucant.org>
2016-09-21 00:23:08 +0100

    move fns within class

--- server_vm.py
+++ server_vm.py
@@ -19,22 +19,6 @@ def poll_until_running(reservation):
         time.sleep(11)
         status = instance.update()
 
-def launch_instance(name):
-    """Create an instance, wait for it to come up, and tag it with a name"""
-
-    config = get_config()
-    security_options = config["security_options"]
-    conn = boto.ec2.connect_to_region(config["region"], **security_options)
-    reservation = conn.run_instances(
-        config["ami_id"],
-        key_name=config["key_name"],
-        security_groups=config["security_groups"]
-    )
-
-    instance = reservation.instances[0]
-    poll_until_running(reservation)
-    conn.create_tags(instance.id, {"Name": name})
-    return instance
 
 def setup_remote_repository(instance, git_reference):
     key_file = "/home/mk270/.ssh/tmtkeys.pem"
@@ -75,5 +59,22 @@ class ServerVM(object):
         self.git_reference = git_reference
 
     def run(self):
-        instance = launch_instance(self.name)
+        instance = self.launch_instance()
         setup_remote_repository(instance, self.git_reference)
+
+    def launch_instance(self):
+        """Create an instance, wait for it to come up, and tag it with a name"""
+
+        config = get_config()
+        security_options = config["security_options"]
+        conn = boto.ec2.connect_to_region(config["region"], **security_options)
+        reservation = conn.run_instances(
+            config["ami_id"],
+            key_name=config["key_name"],
+            security_groups=config["security_groups"]
+        )
+
+        instance = reservation.instances[0]
+        poll_until_running(reservation)
+        conn.create_tags(instance.id, {"Name": self.name})
+        return instance
