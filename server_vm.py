863911ed26d687336086a44debb7357641c22178
Martin Keegan <martin@no.ucant.org>
2016-09-21 00:28:24 +0100

    move fns into class

--- server_vm.py
+++ server_vm.py
@@ -9,39 +9,6 @@ import subprocess
 from configuration import get_config
 
 
-def setup_remote_repository(instance, git_reference):
-    key_file = "/home/mk270/.ssh/tmtkeys.pem"
-    cmds = [ "mkdir -p ~/.ssh",
-             "ssh-keyscan git.unipart.io >> ~/.ssh/known_hosts",
-             "mkdir -p ~/repos"
-             ]
-    ssh = boto.manage.cmdshell.sshclient_from_instance(
-        instance,
-        key_file,
-        user_name="ubuntu")
-    for cmd in cmds:
-        print "CMD", cmd
-        status, stderr, stdout = ssh.run(cmd)
-        assert status == 0
-
-    logname = os.environ["LOGNAME"] # YES, really
-    repo = "ssh://%s@git.unipart.io//home/scm/hawkeye.git" % logname
-
-    subprocess.check_call([
-        "ssh", "-A", "-l", "ubuntu",
-        "-i", key_file,
-        "-o", "UserKnownHostsFile=/dev/null",
-        "-o", "StrictHostKeyChecking=no",
-        instance.ip_address,
-        "git", "clone", repo, "repos/hawkeye"
-    ])
-
-    cmds = [ "git -C repos/hawkeye checkout %s" % git_reference ]
-    for cmd in cmds:
-        print "CMD", cmd
-        status, stderr, stdout = ssh.run(cmd)
-        assert status == 0
-
 class ServerVM(object):
     def __init__(self, name, git_reference):
         self.name = name
@@ -49,7 +16,7 @@ class ServerVM(object):
 
     def run(self):
         instance = self.launch_instance()
-        setup_remote_repository(instance, self.git_reference)
+        self.setup_remote_repository(instance)
 
     def launch_instance(self):
         """Create an instance, wait for it to come up, and tag it with a name"""
@@ -78,3 +45,36 @@ class ServerVM(object):
             print instance, status
             time.sleep(11)
             status = instance.update()
+
+    def setup_remote_repository(self, instance):
+        key_file = "/home/mk270/.ssh/tmtkeys.pem"
+        cmds = [ "mkdir -p ~/.ssh",
+                 "ssh-keyscan git.unipart.io >> ~/.ssh/known_hosts",
+                 "mkdir -p ~/repos"
+                 ]
+        ssh = boto.manage.cmdshell.sshclient_from_instance(
+            instance,
+            key_file,
+            user_name="ubuntu")
+        for cmd in cmds:
+            print "CMD", cmd
+            status, stderr, stdout = ssh.run(cmd)
+            assert status == 0
+
+        logname = os.environ["LOGNAME"] # YES, really
+        repo = "ssh://%s@git.unipart.io//home/scm/hawkeye.git" % logname
+
+        subprocess.check_call([
+            "ssh", "-A", "-l", "ubuntu",
+            "-i", key_file,
+            "-o", "UserKnownHostsFile=/dev/null",
+            "-o", "StrictHostKeyChecking=no",
+            instance.ip_address,
+            "git", "clone", repo, "repos/hawkeye"
+        ])
+
+        cmds = [ "git -C repos/hawkeye checkout %s" % self.git_reference ]
+        for cmd in cmds:
+            print "CMD", cmd
+            status, stderr, stdout = ssh.run(cmd)
+            assert status == 0
